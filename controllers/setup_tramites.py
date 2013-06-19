2d4208c21fead0d7630c20c104c03644e8574166
Mauro Schiavone <mauroschiavone2012@gmail.com>
2013-06-19 19:35:19 -0300

    agrego datos a estados y actualizamos expedientes

--- controllers/setup_tramites.py
+++ controllers/setup_tramites.py
@@ -43,9 +43,9 @@ def cargar_expedientes():
     # Insertamos los expedientes requeridos
     
     db.expedientes.insert(alumnoid=1, tramiteid=1, estadoid=1, mensaje="Certificado de Alumno Regular")
-    db.expedientes.insert(alumnoid=1, tramiteid=1, estadoid=1, mensaje="Certificado de Titulo en Tramite")
-    db.expedientes.insert(alumnoid=1, tramiteid=1, estadoid=1, mensaje="Certificado de Reincorporacion")
-    db.expedientes.insert(alumnoid=1, tramiteid=1, estadoid=1, mensaje="Certificado de Materias Aprobadas")
+    db.expedientes.insert(alumnoid=1, tramiteid=2, estadoid=2, mensaje="Certificado de Titulo en Tramite")
+    db.expedientes.insert(alumnoid=1, tramiteid=3, estadoid=3, mensaje="Certificado de Reincorporacion")
+    db.expedientes.insert(alumnoid=1, tramiteid=4, estadoid=4, mensaje="Certificado de Materias Aprobadas")
     
     
     response.view="generic.html"
@@ -53,3 +53,24 @@ def cargar_expedientes():
     # Devuelvo una consulta del contenido de la tabla
    
     return {'filas': db(db.expedientes.expedienteid>0).select()}
+    
+    
+
+def cargar_estados():
+    
+    db(db.estados.estadoid>0).delete()  
+
+    # Insertamos los estados requeridos
+    
+    db.estados.insert(estadoid=1, descripcion="iniciado", activo="true")
+    db.estados.insert(estadoid=2, descripcion="confeccionado", activo="true")
+    db.estados.insert(estadoid=3, descripcion="autenticado", activo="true")
+    db.estados.insert(estadoid=4, descripcion="listo para entregar", activo="true")
+    db.estados.insert(estadoid=5, descripcion="finalizado", activo="true")
+    
+    
+    response.view="generic.html"
+    
+    # Devuelvo una consulta del contenido de la tabla
+   
+    return {'filas': db(db.estados.estadoid>0).select()}
